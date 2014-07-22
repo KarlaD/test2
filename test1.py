66152b641879377999a4a0de96426e48e53f6ccf
KarlaD <krlam_09@hotmail.com>
2014-07-22 08:44:04 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,24 @@
-from mininet.net import Mininet
-from mininet.topolib import TreeTopo
-tree4=TreeTopo(depth=2,fanout=2)
-net=Mininet(topo=tree4)
-net.start()
-h1,h4=net.hosts[0], net.hosts[3]
-print h1.cmd('ping -c1 % h4.IP())
-net.stop
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
